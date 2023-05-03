126387d0443f4a66499b4325927d9265fca3e5e5
Leandro <perezbarahonaleandro@gmail.com>
2023-05-02 22:28:30 -0400

    mobile navbar and burger menu done!

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,5 +1,27 @@
-export default function BurgerMenu() {
+import {IoMdClose} from 'react-icons/io';
+import {AiFillCaretDown} from 'react-icons/ai'
+import '../assets/burgerMenu.css';
+
+export default function BurgerMenu({setBurgerMenuState}) {
+
+  const sections = ['Currency','Items','Accounts','Services','Swap','Sell Currency','Sell Item','Sell Acount']
+
   return (
-    <div>BurgerMenu</div>
+    <div className="burgerMenu">
+      <div className='burgerMenu_header'>
+        <IoMdClose className='burgerMenu_header-closeIcon' onClick={()=>setBurgerMenuState(false)} />
+        <span>Menu</span>
+      </div>
+      <ul className='burgerMenu_sections'>
+        {
+          sections.map(section=>(
+            <li className='burgerMenu_sections-section'>
+              <span>{section}</span>
+              <AiFillCaretDown />
+            </li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,19 @@ import '../assets/navbar.css';
 import {RxHamburgerMenu} from 'react-icons/rx';
 import logo from '../assets/chicks-logo-large.svg';
 import {FaUser} from 'react-icons/fa';
+import BurgerMenu from './BurgerMenu';
+import { useState } from 'react';
 
 export default function Navbar() {
+
+  const [burgerMenuState,setBurgerMenuState] = useState(false);
+
   return (
     <nav className='navbar'>
         <div className='navbar_left'>
-            <RxHamburgerMenu className='navbar_left-burgerMenu' />
+            <RxHamburgerMenu className='navbar_left-burgerMenuIcon' onClick={()=>setBurgerMenuState(true)} />
             <img src={logo} alt='logo' className='navbar_left-logo' />
+            {burgerMenuState && <BurgerMenu setBurgerMenuState={setBurgerMenuState} />}
         </div>
         <button className='navbar_signInBtn'>
             SIGN IN
