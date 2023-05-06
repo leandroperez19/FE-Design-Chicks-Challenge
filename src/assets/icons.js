856f089f75941e3234c337106c5e7ebb0bd63543
Leandro <perezbarahonaleandro@gmail.com>
2023-05-05 23:46:28 -0400

    pagination added

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,9 +1,9 @@
 import {RxHamburgerMenu} from 'react-icons/rx';
-import {FaUser,FaChevronDown,FaGamepad,FaSort} from 'react-icons/fa';
+import {FaUser,FaChevronDown,FaChevronLeft,FaChevronRight,FaGamepad,FaSort} from 'react-icons/fa';
 import {HiShoppingCart} from 'react-icons/hi'
 import {IoMdClose} from 'react-icons/io';
 import {AiFillCaretDown,AiOutlineSearch} from 'react-icons/ai'
 import {GiBroadsword,GiPaperBagCrumpled,GiFeather} from 'react-icons/gi';
 import {VscSettings} from 'react-icons/vsc';
 
-export {RxHamburgerMenu,FaUser,FaChevronDown,HiShoppingCart,IoMdClose,AiFillCaretDown,GiBroadsword,AiOutlineSearch,GiPaperBagCrumpled,GiFeather,VscSettings,FaGamepad,FaSort}
+export {RxHamburgerMenu,FaUser,FaChevronDown,HiShoppingCart,IoMdClose,AiFillCaretDown,GiBroadsword,AiOutlineSearch,GiPaperBagCrumpled,GiFeather,VscSettings,FaGamepad,FaSort,FaChevronRight,FaChevronLeft}
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,42 @@
+import { useEffect, useState } from 'react';
+import {FaChevronLeft, FaChevronRight} from '../assets/icons';
+import '../stylesheets/pagination.css';
 
 export default function Pagination() {
+
+  const totalPages = 11;
+  const [pages,setPages] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [currentPage, setCurrentPage] = useState(1);
+
+  function createPages(){
+    let arr = [];
+    for (let i = 1; i <= totalPages; i++) {
+      arr.push(i)
+    }
+    setPages(arr.slice(currentPage - 1,currentPage + 2))
+  }
+  useEffect(()=>{
+    createPages()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+
   return (
-    <div>Pagination</div>
+    <div className='pagination'>
+      <span className='pagination_chevronLeft'>
+        <FaChevronLeft  />
+      </span>
+      {pages.map(page=>(
+          <span className={`page ${page === currentPage && 'currentPage'}`} >
+            {page}
+          </span>
+        ))
+      }
+      <span className='page'>...</span>
+      <span className='page'>{totalPages}</span>
+      <span className='pagination_chevronRight'>
+        <FaChevronRight  />
+      </span>
+    </div>
   )
 }
